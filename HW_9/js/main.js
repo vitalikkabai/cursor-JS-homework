00124cdfd70c0d7e997d931660bb747de2a43610
Vitalik Kabai <vitalikkabai@gmail.com>
2020-08-19 05:05:16 +0300

    HW#9 DOM finaly

---
+++ HW_9/js/main.js
@@ -0,0 +1,28 @@
+const container = document.querySelector('.container');
+
+const generateRandomColor = () => {
+    return `#${(Math.ceil(Math.random()*16777215)).toString(16)}`
+}
+
+
+const generateBlocks = () => {
+    for (var j = 0; j < 5; j++) {
+        for (var i = 0; i < 5; i++) {
+            const firstLine = document.createElement("div");
+            firstLine.classList.add("colorBlock");
+            firstLine.style.cssText = `width: 50px; height: 50px; background-color: ${generateRandomColor()}`;
+            container.append(firstLine);
+        }
+    }
+}
+
+generateBlocks();
+
+const generateBlocksInterval = () => {
+    const colorBlockList = document.getElementsByClassName("colorBlock");
+    for (var i = 0; i < colorBlockList.length; i++) {
+        colorBlockList[i].style.backgroundColor = `${generateRandomColor()}`;
+    }
+}
+
+setInterval(generateBlocksInterval, 1000);
