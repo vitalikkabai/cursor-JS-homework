934e27df56c44cb9b035f42735ba9b1236f261df
Vitalik Kabai <vitalikkabai@gmail.com>
2020-09-07 22:30:08 +0300

    HW_11

---
+++ HW_11/js/main.js
@@ -0,0 +1,34 @@
+const getChineseChar = () => String.fromCharCode(String(Date.now()).slice(-5));
+
+
+const getRandomChinese = (length) => {
+    return new Promise((resolve, reject) => {
+        if (length >= 0) {
+            let result = "";
+            let count = 0
+            for (var i = 0; i < length; i++) {
+
+
+                setTimeout(() => {
+                    result += getChineseChar();
+                    count++;
+                    if (count === length) {
+                        resolve(result);
+                    }
+                }, 50 * (i + 1));
+            }
+        } else {
+        	reject();	
+        }
+
+
+
+    });
+}
+
+getRandomChinese(4).then(result => {
+    console.log('Done!');
+    console.log(result);
+}).catch(()=>{
+	console.log("This length is uncorrectable!");
+})
