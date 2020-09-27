e29a4517f0cd7a3b0fd056c05b40bede25eab492
Vitalik Kabai <vitalikkabai@gmail.com>
2020-09-27 21:17:48 +0300

    Updates

---
+++ precache-manifest.46fc7d21728b197f4c724c2de1b05d1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d03cabe880874555485b7d41c094445e",
+    "url": "/vitalikkabai/cursor-JS-homework/HW_15.git/index.html"
+  },
+  {
+    "revision": "b408aff7e8afbd865d5a",
+    "url": "/vitalikkabai/cursor-JS-homework/HW_15.git/static/css/main.05002c4c.chunk.css"
+  },
+  {
+    "revision": "a4b3543327389c06c9d6",
+    "url": "/vitalikkabai/cursor-JS-homework/HW_15.git/static/js/2.6eaadabc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vitalikkabai/cursor-JS-homework/HW_15.git/static/js/2.6eaadabc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b408aff7e8afbd865d5a",
+    "url": "/vitalikkabai/cursor-JS-homework/HW_15.git/static/js/main.7ed5f9f0.chunk.js"
+  },
+  {
+    "revision": "4e7afd819ee5738cc408",
+    "url": "/vitalikkabai/cursor-JS-homework/HW_15.git/static/js/runtime-main.ad4abdd3.js"
+  },
+  {
+    "revision": "96c76035f23fa0da55e9e4c93d746524",
+    "url": "/vitalikkabai/cursor-JS-homework/HW_15.git/static/media/twitter-verified-badge.96c76035.svg"
+  }
+]);
