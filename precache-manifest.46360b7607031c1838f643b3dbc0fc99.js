36a772d04f4048166121dffcee078ba25852ff50
Vitalik Kabai <vitalikkabai@gmail.com>
2020-09-30 03:00:09 +0300

    Updates

---
+++ precache-manifest.46360b7607031c1838f643b3dbc0fc99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1af13c24c5714fe7983ffda296b4bc61",
+    "url": "/cursor-react-homework/react-hw/index.html"
+  },
+  {
+    "revision": "7e6dd7e22cbafb1337df",
+    "url": "/cursor-react-homework/react-hw/static/css/main.05002c4c.chunk.css"
+  },
+  {
+    "revision": "a4b3543327389c06c9d6",
+    "url": "/cursor-react-homework/react-hw/static/js/2.6eaadabc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cursor-react-homework/react-hw/static/js/2.6eaadabc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e6dd7e22cbafb1337df",
+    "url": "/cursor-react-homework/react-hw/static/js/main.560196e1.chunk.js"
+  },
+  {
+    "revision": "d40a9bfc70847a104cf7",
+    "url": "/cursor-react-homework/react-hw/static/js/runtime-main.1854f136.js"
+  },
+  {
+    "revision": "96c76035f23fa0da55e9e4c93d746524",
+    "url": "/cursor-react-homework/react-hw/static/media/twitter-verified-badge.96c76035.svg"
+  }
+]);
